5bc8daa985042549eed079b4c2b5ce462a849e3c
bernardogrodrigues <bernardo.rodrigues2005@gmail.com>
2023-12-05 00:04:26 +0000

    Update random_walk.py
    Conclusão por hoje
    Extra: adição de aleatoriedade ao incremento na posição de uma partícula com a função uniform()

--- random_walk.py
+++ random_walk.py
@@ -1,9 +1,25 @@
 import random
+from random import uniform
 import matplotlib.pyplot as plt
+
+class Particle:
+  def __init__(self, position) -> None:
+    self.positions = [position]
+  
+  def update_positions(self, inc) -> None:
+    self.positions.append(self.positions[-1]+inc)
 
 def random_walk(num_steps:int, prob_right:float, num_particles:int):
   
-  #in progress...
+  particles = [Particle(0)] * num_particles
+
+  for i in range(num_steps):
+    for particle in particles:
+      if random.random() <= prob_right:
+        particle.update_positions(uniform(0, 5))
+      else: particle.update_positions(uniform(-5, 0))
+
+  particle_paths = [p.positions for p in particles]
 
   create_plot(num_steps, particle_paths)
 
@@ -17,12 +33,12 @@ def create_plot(num_steps, particle_paths):
     plt.plot(particle_path, time)
 
   plt.title('Random Walk - N particles')
-  plt.xlabel('Time')
-  plt.ylabel('Position')
+  plt.xlabel('Position')
+  plt.ylabel('Time')
   plt.show()
 
-num_steps = 100
+num_steps = 10
 prob_right = 0.5
-num_particles = 10
+num_particles = 1000
 
 random_walk(num_steps, prob_right, num_particles)
